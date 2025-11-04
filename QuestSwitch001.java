6832bbd48cb0363635a06b570d6bfb287f9b15e1
wafaaidan <wafa.aidan@gmail.com>
2025-11-04 19:29:25 +0700

    membuat conditional: user confirm

--- QuestSwitch001.java
+++ QuestSwitch001.java
@@ -3,5 +3,13 @@ import java.util.Scanner;
 public class QuestSwitch001 {
     public static void main(String[] args) {
         Scanner MeinScan = new Scanner(System.in);
+
+        System.out.print("Masukkan key untuk mengakses:  ");
+        String confirm = MeinScan.nextLine();
+
+        if(confirm.toLowerCase().equals("asdos25")) {
+        } else {
+            System.out.println("Maaf, Anda tidak memiliki akses");
+        }
     }
 }
